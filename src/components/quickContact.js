97e2409671d67e6974879c0e06365a6989ae5911
Marcin Zogrodnik <m.zog@icloud.com>
2020-10-23 11:21:43 +0200

    Remove input from QuickContact

--- src/components/quickContact.js
+++ src/components/quickContact.js
@@ -19,9 +19,6 @@ const QuickContact = () => {
             <label htmlFor="phone-email">
               <input type="text" name="phone-email" id="phone-email" placeholder="np. 739907919"/>
             </label>
-            <label htmlFor="path">
-              <input type="hidden" name="path" id="path" value={`${window.location.pathname}`}/>
-            </label>
           </div>
           <div className="form-input">
             <label htmlFor="submit">
