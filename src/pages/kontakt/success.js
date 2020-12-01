16b78bac91323cf4ff5b684f790b2ccbd5d0b5e6
Marcin Zogrodnik <m.zog@icloud.com>
2020-12-01 13:56:20 +0100

    DEC2020 v3.3

--- src/pages/kontakt/success.js
+++ src/pages/kontakt/success.js
@@ -6,9 +6,12 @@ import {Link} from 'gatsby'
 const Success = () => (
     <Layout>
         <SEO title="Wiadomość wysłana!" description="Twoja wiadomość została wysłana" />
-        <section className="container page">
+        <section className="container page success">
+            <h1>Dziękuję za wiadomość</h1>
 
-            <Link to="/" className="btn">Powrót na stronę główną</Link>
+            <p>Twoja wiadomość już jest u mnie na skrzynce, zapoznam się z nią i odpiszę lub oddzwonię.</p>
+
+            <Link to="/" className="btn primary">Powrót na stronę główną</Link>
         </section>
     </Layout>
 );
