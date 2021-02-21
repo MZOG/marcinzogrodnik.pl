74eb860c59d04a38bed0311390176a4145d33cb9
Marcin Zogrodnik <m.zog@icloud.com>
2021-02-21 12:55:51 +0100

    wycena strony

--- src/pages/darmowa-wycena-strony.js
+++ src/pages/darmowa-wycena-strony.js
@@ -13,33 +13,9 @@ const DarmowaWycenaStrony = () => {
       <section className="container page">
         <h1>Darmowa wycena strony</h1>
 
-        <p>
-          Bardzo częstym pytaniem, które znajduję w swojej skrzynce pocztowej,
-          to "ile kosztuje strona internetowa?" Nie posiadam stałego cennika
-          stron internetowych, nie jest to możliwe. Jeżeli spotykasz przedziały
-          cenowe w ofertach innych firm wykonujących strony, to możesz być
-          na 90% pewien, że są to gotowce.
-        </p>
+        <p>Tworzę strony internetowe <strong>WordPress</strong> oraz <strong>JAMstack</strong>. Jeżeli jesteś zainteresowany moimi usługami, lub chciałbyś dowiedzieć się ile mniej więcej kosztuje wykonanie strony internetowej według Twoich wytycznych, to napisz do mnie.</p>
 
-        <p>
-          Moi klienci nigdy nie dostaną gotowca, za każdym razem tworzę unikalny
-          styl graficzny oraz podchodzę do klienta bardzo indywidualnie,
-          traktuję stronę internetową klienta jak swoje dziecko, którym trzeba
-          się cały czas opiekować, i wprowadzać zmiany, zeby się rozwijało.
-        </p>
-
-        <p>
-          Wycena strony internetowej nie jest łatwa, wszystko zależy tak na prawdę
-          od oczekiwań klienta. Jeżeli klient oczekuje rozbudowanej strony, do tego
-          zintegrowanego sklepu, plus płatności, plus galerie itp, to wiadomo, że
-          czas, który poświęce na realizację projektu się wydłuża, prawda?
-        </p>
-
-        <p>Dlatego projekty wyceniam wg. przepracowanych godzin. Gdy już wszystko wiem,
-          czyli mam wszystkie dane na temat podstron, tego czy wykonać logo itp. przystępuję
-          do oszacowania czasu na poszczególne zadania.
-
-        </p>
+        <p>Chciałbym uzyskać jak najwięcej informacji o Twojej stronie np. <strong>branża</strong>, <strong>funkcjonalność</strong>, <strong>strony, które podobają Ci się wizualnie</strong>. Taki punkt zaczepny, abym mógł oszacować czas wykonania projektu.</p>
       </section>
     </Layout>
   )
