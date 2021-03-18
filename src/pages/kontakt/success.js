6f79b6b0527f6059afdd995e40e2e2d694e2e71e
Marcin Zogrodnik <m.zog@icloud.com>
2021-03-18 23:05:20 +0100

    fix seo for pages 6

--- src/pages/kontakt/success.js
+++ src/pages/kontakt/success.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Layout from "../../components/layout";
-import SEO from "../../components/seo";
-import {Link} from 'gatsby'
+import React from "react"
+import Layout from "../../components/layout"
+import SEO from "../../components/seo"
+import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
 
 const Success = () => {
-    const successQuote = useStaticQuery(graphql`
+  const successQuote = useStaticQuery(graphql`
     {
       file(relativePath: { eq: "kontakt-fb.png" }) {
         publicURL
@@ -13,22 +13,28 @@ const Success = () => {
     }
   `)
 
-    return(
+  return (
     <Layout>
-        <SEO
-            title="Wiadomość wysłana!"
-            description="Twoja wiadomość została wysłana"
-            shareImage={successQuote.file.publicURL}
-        />
+      <SEO
+        title="Wiadomość wysłana!"
+        description="Twoja wiadomość została wysłana"
+        shareImage={successQuote.file.publicURL}
+      />
 
-        <section className="container page success">
-            <h1>Dziękuję za wiadomość</h1>
+      <section className="container page success">
+        <h1>Dziękuję za wiadomość</h1>
 
-            <p>Twoja wiadomość już jest u mnie na skrzynce, zapoznam się z nią i odpiszę lub oddzwonię.</p>
+        <p>
+          Twoja wiadomość już jest u mnie na skrzynce, zapoznam się z nią i
+          odpiszę lub oddzwonię.
+        </p>
 
-            <Link to="/" className="btn primary">Powrót na stronę główną</Link>
-        </section>
+        <Link to="/" className="btn primary">
+          Powrót na stronę główną
+        </Link>
+      </section>
     </Layout>
-);
+  )
+}
 
-export default Success;
+export default Success
